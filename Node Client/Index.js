b9c510bcb9f106e7b0d9cee05f57e735aa61fe98
llromulusll <jrommel889@gmail.com>
2023-02-03 23:12:20 -0500

    deleted:    Javascript/TestClient1.js 	new file:   Node Client/Index.js

---
+++ Node Client/Index.js
@@ -0,0 +1,27 @@
+const mqtt = require('mqtt');
+const client = mqtt.connect('localhost', 1883); 
+const topic = 'topic/name';
+const message = 'test message'; 
+
+client.on('connect', () => {
+    console.log(`Is client connected: ${client.connected}`);    
+    if (client.connected === true) {
+        console.log(`message: ${message}, topic: ${topic}`); 
+        // publish message        
+        client.publish(topic, message);
+    }
+
+    // subscribe to a topic
+    client.subscribe(topic);
+});
+
+// receive a message from the subscribed topic
+client.on('message',(topic, message) => {
+    console.log(`message: ${message}, topic: ${topic}`); 
+});
+
+// error handling
+client.on('error',(error) => {
+    console.error(error);
+    process.exit(1);
+});
